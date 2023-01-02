23730a8a2828ea8be10e4ffd3ef5e1d4b207cca2
JunHa-Hwang <hwang_junha@naver.com>
2023-01-02 16:08:57 +0900

    Update add.rs

--- 2_Types/add.rs
+++ 2_Types/add.rs
@@ -1,4 +1,10 @@
 fn main() {
+    /*
+    let을 통해 변수를 바인딩 할 수 있으며 기본적으로 immutable하다.
+    
+    mutable한 변수로 선언하려면 let mut a = 7; 과 같이 선언해주어야한다.
+    */
+    
     let a = 7;  // 컴파일러가 타입 추론
 
     let b:i32 = 70;  // 프로그래머가 직접 지정, i32:integer 32비트 u32:unsigned integer 32비트

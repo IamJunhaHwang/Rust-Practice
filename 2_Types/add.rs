c13ef7af2ae5a1c774fc9ccfc9263dd33201f41e
JunHa-Hwang <hwang_junha@naver.com>
2023-01-02 16:06:36 +0900

    Create add.rs

---
+++ 2_Types/add.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let a = 7;  // 컴파일러가 타입 추론
+
+    let b:i32 = 70;  // 프로그래머가 직접 지정, i32:integer 32비트 u32:unsigned integer 32비트
+    let c = 15i32;
+    let d = 15_i32;
+
+    let e = add(add(a,b), add(c, d));
+
+    println!("a+b+c+d = {}", e);
+}
+
+fn add(i:i32, j:i32) ->i32{  // 함수 정의에 타입 선언은 반드시 필요
+    i+j  // 별도의 return 문이 필요 없음.
+}
+
+/*OUTPUT
+a+b+c+d = 107
+ */
